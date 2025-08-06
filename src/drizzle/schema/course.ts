ee9640c9f48c95d04eac1e9fd4f29ddd125544ae
Adham Ehab <adhamehab2112@gmail.com>
2025-08-06 13:08:09 +0200

    first commit

---
+++ src/drizzle/schema/course.ts
@@ -0,0 +1,14 @@
+import { relations } from "drizzle-orm";
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const CourseTable = pgTable("courses",{
+    id : uuid().primaryKey().defaultRandom(),
+    name : text().notNull(),
+    description : text().notNull(),
+    createdAt : timestamp({withTimezone:true}).notNull().defaultNow(),
+    updatedAt : timestamp({withTimezone:true}).notNull().defaultNow().$onUpdate(()=> new Date()),
+})
+
+export const CourseRelationShips = relations(CourseTable,({one,many})=>({
+    // test : one()
+}) )
